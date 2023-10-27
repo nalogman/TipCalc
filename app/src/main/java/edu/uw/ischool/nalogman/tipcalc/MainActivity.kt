1d4553bf1f8e2ad2d4aa39e35d1439d2ae3cbf40
Noah Logman <nalogman@uw.edu>
2023-10-26 23:45:10 -0700

    Completed Homework

---
+++ app/src/main/java/edu/uw/ischool/nalogman/tipcalc/MainActivity.kt
@@ -0,0 +1,60 @@
+package edu.uw.ischool.nalogman.tipcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var editTextAmount: EditText
+    private lateinit var buttonTip: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editTextAmount = findViewById(R.id.editTextAmount)
+        buttonTip = findViewById(R.id.buttonTip)
+
+        // Apply an InputFilter to allow only numbers and format as USD currency
+        val currencyFilter = InputFilter { source, start, end, dest, dstart, dend ->
+            val newInput = dest.toString().substring(0, dstart) + source.subSequence(start, end) + dest.toString().substring(dend)
+            if (newInput.matches(Regex("^\\d+(\\.\\d{0,2})?$"))) {
+                null
+            } else {
+                ""
+            }
+        }
+        editTextAmount.filters = arrayOf<InputFilter>(currencyFilter)
+
+        // Add a TextWatcher to enable the "Tip" button when data is entered
+        editTextAmount.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // Enable the "Tip" button when data is entered
+                val isDataEntered = s?.isNotEmpty() ?: false
+                buttonTip.isEnabled = isDataEntered
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // Add an OnClickListener to the "Tip" button to calculate and display the tip
+        buttonTip.setOnClickListener {
+            val serviceCharge = editTextAmount.text.toString().toDoubleOrNull()
+            if (serviceCharge != null) {
+                val tipAmount = serviceCharge * 0.15
+                val formattedTip = String.format("$%.2f", tipAmount)
+                Toast.makeText(this, "Tip: $formattedTip", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please enter a valid service charge", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
